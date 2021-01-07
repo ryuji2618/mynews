9265824803f62200abe4092a460697e632143c8e
sasaki ryuji <x.0x.0x.0x.0x.0.x@yahoo.ne.jp>
2021-01-07 12:54:43 +0000

    更新

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
+
+    
 
 Auth::routes();
